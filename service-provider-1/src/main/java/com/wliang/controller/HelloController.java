02e4d06f968c9175bed9cdfe58a9033e9212bd90
wangl3 <1172638736@qq.com>
2022-08-10 20:10:06 +0800

    首次提交

---
+++ service-provider-1/src/main/java/com/wliang/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.wliang.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wliang
+ * @description TODO
+ * @date 2022-08-09 20:15
+ */
+@RestController
+public class HelloController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/hi")
+    public String hi(@RequestParam(value = "name", defaultValue = "provider", required = false) String name) {
+        return "hi " + name + " i'm service provider 1,my port: " + port;
+    }
+}
