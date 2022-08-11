6f0294bb2c3faea68f24b1e05c308e8ff035fa35
wangl3 <1172638736@qq.com>
2022-08-11 19:46:10 +0800

    新增配置中心项目

---
+++ nacos-config/src/main/java/com/wliang/config/EmailConfig.java
@@ -0,0 +1,21 @@
+package com.wliang.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wliang
+ * @description 邮箱配置
+ * @date 2022-08-11 19:29
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "email")
+public class EmailConfig {
+    private String url;
+
+    private String username;
+
+    private String password;
+}
---
+++ nacos-config/src/main/java/com/wliang/controller/ConfigController.java
@@ -0,0 +1,25 @@
+package com.wliang.controller;
+
+import com.wliang.config.EmailConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wliang
+ * @description TODO
+ * @date 2022-08-11 19:33
+ */
+@RestController
+@RefreshScope
+public class ConfigController {
+
+    @Autowired
+    private EmailConfig emailConfig;
+
+    @GetMapping("/emailUrl")
+    public String emailUrl(){
+        return emailConfig.getUrl();
+    }
+}
